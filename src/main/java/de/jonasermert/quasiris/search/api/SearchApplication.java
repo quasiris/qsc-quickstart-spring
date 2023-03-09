aa3028dbd790c6d8b7be1a3cf5dc449aff2d218e
Jonas <ermert.jonas@yahoo.de>
2023-03-09 11:37:20 +0100

    09.03
    09.03

--- src/main/java/de/jonasermert/quasiris/search/api/SearchApplication.java
+++ src/main/java/de/jonasermert/quasiris/search/api/SearchApplication.java
@@ -2,8 +2,10 @@ package de.jonasermert.quasiris.search.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class SearchApplication {
 
 	public static void main(String[] args) {
