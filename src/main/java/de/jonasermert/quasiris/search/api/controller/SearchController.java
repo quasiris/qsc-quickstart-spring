6083b7607303f7a49bce88acdb6504ddc0f2b185
Jonas Ermert <ermert.jonas@yahoo.de>
2023-04-03 00:49:21 +0200

    03.04.2023

--- src/main/java/de/jonasermert/quasiris/search/api/controller/SearchController.java
+++ src/main/java/de/jonasermert/quasiris/search/api/controller/SearchController.java
@@ -1,6 +1,7 @@
 package de.jonasermert.quasiris.search.api.controller;
 
 import com.quasiris.qsf.dto.response.SearchResponse;
+import com.quasiris.qsf.dto.response.SearchResult;
 import de.jonasermert.quasiris.search.api.service.SearchService;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @Controller("SearchController")
@@ -25,6 +27,21 @@ public class SearchController {
         model.addAttribute("searchResults", searchResults);
         return "index";
     }
+
+
+    /*
+
+    // Anzahl der gefundenen Suchergebnisse ausgeben
+    @GetMapping("/")
+    public String search(@RequestParam(name = "query") String query, Model model) {
+        List<SearchResult> results = searchService.search(query);
+        int count = results.size(); // zählt die Anzahl der Suchergebnisse
+        model.addAttribute("results", results);
+        model.addAttribute("query", query);
+        model.addAttribute("count", count); // fügt die Anzahl der Suchergebnisse dem Model hinzu
+        return "search";
+    }
+*/
 
 }
 
--- src/main/java/de/jonasermert/quasiris/search/api/service/SearchService.java
+++ src/main/java/de/jonasermert/quasiris/search/api/service/SearchService.java
@@ -1,8 +1,11 @@
 package de.jonasermert.quasiris.search.api.service;
 
 import com.quasiris.qsf.dto.response.SearchResponse;
+import com.quasiris.qsf.dto.response.SearchResult;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 
 @Service("SearchService")
@@ -15,6 +18,12 @@ public class SearchService {
         return searchResults;
     }
 
+    // Anzahl der gefundenen Suchergebnisse ausgeben
+    /*
+    public List<SearchResult> search(String query) {
+        return null;
+    }
+     */
 }
 
 
